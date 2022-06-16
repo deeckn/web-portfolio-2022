e0eb62ebc9a488be0d39648793170d8db31f9c68
Dee Deesit <40157947+deeckn@users.noreply.github.com>
2022-06-16 22:50:07 +0700

    Change display to flex on large screens

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -8,7 +8,7 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ icon, title, description }) => {
     return (
-        <div className='flex flex-col items-center p-12 bg-white mb-8 max-w-sm'>
+        <div className='flex flex-col items-center p-12 bg-white mb-8 max-w-sm h-full'>
             {icon}
             <h3 className='font-bold text-2xl mt-8 mb-4 text-project-card-title'>{title}</h3>
             <p className='text-paragraph md:text-lg lg:text-xl'>{description}</p>
--- src/Pages/Home/Projects.tsx
+++ src/Pages/Home/Projects.tsx
@@ -5,10 +5,10 @@ import { CgWebsite } from 'react-icons/cg'
 
 const Projects: React.FC = () => {
     return (
-        <section className='bg-gray-bg px-8 py-12 flex flex-col items-center sm:px-8 md:px-36 lg:px-72 xl:px-[24rem] 2xl:px-[36rem] md:py-24 lg:py-36'>
+        <section className='bg-gray-bg px-8 py-12 flex flex-col items-center sm:px-8 md:px-36 md:py-24 lg:py-36'>
             <h2 className='text-4xl font-bold text-gray-dark mb-4'>Projects</h2>
             <p className='text-paragraph md:text-lg lg:text-xl'>Projects that I did throughout my Software Engineering journey.</p>
-            <ul className='mt-8'>
+            <ul className='mt-8 xl:flex xl:gap-4'>
                 <li>
                     <ProjectCard
                         title='GIN ARAI DEE'
@@ -28,7 +28,7 @@ const Projects: React.FC = () => {
                         icon={<FaBook className='text-pink' size={40} />} />
                 </li>
             </ul>
-            <button className='bg-pink text-white px-8 py-2 border-solid border-2 border-pink font-bold'>PROJECTS</button>
+            <button className='bg-pink text-white px-8 py-2 border-solid border-2 border-pink font-bold xl:mt-8'>PROJECTS</button>
         </section>
     )
 }

bd34729a456aedec1ff2f9e13f1b3bc48e66d00a
Dee Deesit <40157947+deeckn@users.noreply.github.com>
2022-06-28 14:14:20 +0700

    Add scroll to top and link path

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,17 +4,22 @@ import { headerThreeFontSize } from '../theme'
 import ContactLink from './ContactLink'
 
 const Footer: React.FC = () => {
+    const NavlinkStyles = 'text-xl text-white hover:bg-gray duration-300 rounded-lg hover:px-4 hover:py-2 dark:hover:bg-dark'
+    const scrollToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' })
+
     return (
         <footer className={`bg-dark pb-20 dark:bg-gray 2xl:px-96 xl:px-60 xl:pt-24 lg:px-32 md:px-24 md:pt-16 sm:p-16 py-12 px-8`}>
             <div className='flex md:gap-36 gap-16 flex-col md:flex-row'>
                 <section>
                     <h3 className={`mb-8 font-bold text-white ${headerThreeFontSize}`}>Navigate<span className='text-secondary'>.</span></h3>
-                    <ul>
-                        <li className='mt-4'><Link to='/' className='text-xl text-white hover:bg-gray duration-300 rounded-lg hover:px-4 hover:py-2 dark:hover:bg-dark'>Home</Link></li>
-                        <li className='mt-4'><Link to='/' className='text-xl text-white hover:bg-gray duration-300 rounded-lg hover:px-4 hover:py-2 dark:hover:bg-dark'>About</Link></li>
-                        <li className='mt-4'><Link to='/' className='text-xl text-white hover:bg-gray duration-300 rounded-lg hover:px-4 hover:py-2 dark:hover:bg-dark'>Projects</Link></li>
-                        <li className='mt-4'><Link to='/' className='text-xl text-white hover:bg-gray duration-300 rounded-lg hover:px-4 hover:py-2 dark:hover:bg-dark'>Contact</Link></li>
-                    </ul>
+                    <nav>
+                        <ul>
+                            <li className='mt-4'><Link to='/' className={NavlinkStyles} onClick={scrollToTop}>Home</Link></li>
+                            <li className='mt-4'><Link to='/about' className={NavlinkStyles} onClick={scrollToTop}>About</Link></li>
+                            <li className='mt-4'><Link to='/projects' className={NavlinkStyles} onClick={scrollToTop}>Projects</Link></li>
+                            <li className='mt-4'><Link to='/contact' className={NavlinkStyles} onClick={scrollToTop}>Contact</Link></li>
+                        </ul>
+                    </nav>
                 </section>
 
                 <section>

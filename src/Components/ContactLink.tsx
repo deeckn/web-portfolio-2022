27cf1ba9e169bef4ac5c99573a20cd8a645a4f57
Dee Deesit <40157947+deeckn@users.noreply.github.com>
2022-06-30 22:58:49 +0700

    Contact page complete

--- src/Components/ContactLink.tsx
+++ src/Components/ContactLink.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { FaLinkedin, FaInstagram, FaGithubSquare } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
-import { paragraphFontSize } from '../theme'
+import { headerFourFontSize, headerThreeFontSize, paragraphFontSize } from '../theme'
 
 
 interface IContactLinkProps {
     variant: 'email' | 'linkedin' | 'instagram' | 'github'
     text: string
     url: string
-    size: 'body' | 'footer'
+    size: 'body' | 'footer' | 'page'
 }
 
 const ContactLink: React.FC<IContactLinkProps> = ({ variant, text, url, size }) => {
-    const iconSize = size === 'body' ? 60 : 30
+    let iconSize = size === 'body' ? 60 : 30
 
-    let icon;
+    let icon
     switch (variant) {
         case 'email':
             icon = <HiOutlineMail size={iconSize} />
@@ -28,6 +28,34 @@ const ContactLink: React.FC<IContactLinkProps> = ({ variant, text, url, size }) 
         case 'github':
             icon = <FaGithubSquare size={iconSize} />
             break
+    }
+
+    if (size === 'page') {
+        let icon
+        const iconSize = 'lg:text-7xl md:text-6xl sm:text-5xl text-4xl'
+        switch (variant) {
+            case 'email':
+                icon = <HiOutlineMail className={iconSize} />
+                break
+            case 'linkedin':
+                icon = <FaLinkedin className={iconSize} />
+                break
+            case 'instagram':
+                icon = <FaInstagram className={iconSize} />
+                break
+            case 'github':
+                icon = <FaGithubSquare className={iconSize} />
+                break
+        }
+
+        return (
+            <li className={`my-4 xl:my-8 text-dark dark:text-smoke hover:underline`}>
+                <a href={url} target={"_blank"} className='flex items-center gap-2 xl:gap-4'>
+                    {icon}
+                    <p className={`${headerFourFontSize}`}>{text}</p>
+                </a>
+            </li>
+        )
     }
 
     return (
--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { headerOneFontSize, headerThreeFontSize, sectionPadding } from '../../theme'
+import { motion } from 'framer-motion'
+import { DarkModeContext } from '../../Context/DarkModeContext'
+import ContactLink from '../../Components/ContactLink'
 
 const Contact: React.FC = () => {
+    const { darkMode, toggleDarkMode } = useContext(DarkModeContext)
+
     return (
-        <div>Contact</div>
+        <>
+            <section className={`${sectionPadding} flex flex-col items-center bg-smoke dark:bg-dark`}>
+                <h1 className={`${headerOneFontSize} font-bold text-opacity-0 text-gray`}>
+                    <span className='bg-clip-text bg-gradient-to-r from-primary to-secondary'>Contact</span>
+                </h1>
+                <h3 className={`${headerThreeFontSize} font-bold text-gray mt-8 dark:text-white`}>Let's Work Together</h3>
+                <div className='px-1 py-2 dark:bg-white w-24 rounded-full bg-gray mt-16'>
+                    <motion.div
+                        className='w-8 h-8 rounded-full cursor-pointer'
+                        variants={{
+                            on: { x: "+170%", backgroundColor: "#F46B45" },
+                            off: { x: "+10%", backgroundColor: "#F5F5F5" },
+                        }}
+                        animate={darkMode ? "on" : "off"}
+                        onClick={toggleDarkMode}></motion.div>
+                </div>
+            </section>
+
+            <section className={`${sectionPadding} flex flex-col items-center bg-smoke dark:bg-dark pb-32`}>
+                <ul className='flex flex-col xl:flex-row font-bold items-center xl:gap-16'>
+                    <ContactLink
+                        variant='linkedin'
+                        text='Chakrin Deesit'
+                        url='https://www.linkedin.com/in/chakrin-deesit-076804209'
+                        size='page' />
+
+                    <ContactLink
+                        variant='email'
+                        text='ckn.deesit@gmail.com'
+                        url='mailto:ckn.deesit@gmail.com?'
+                        size='page' />
+                </ul>
+
+                <ul className='flex flex-col xl:flex-row font-bold items-center xl:gap-16'>
+                    <ContactLink
+                        variant='instagram'
+                        text='@dee.ckn'
+                        url='https://www.instagram.com/dee.ckn/'
+                        size='page' />
+
+                    <ContactLink
+                        variant='github'
+                        text='deeckn'
+                        url='https://github.com/deeckn'
+                        size='page' />
+                </ul>
+            </section>
+        </>
     )
 }
 

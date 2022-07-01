afb6ab47d360ffb49fbd6623bb696bb41b3d358d
Dee Deesit <40157947+deeckn@users.noreply.github.com>
2022-07-01 23:48:59 +0700

    Adjust section padding and font sizes

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,6 @@
-export const sectionPadding = '2xl:px-96 2xl:pt-40 xl:px-60 xl:pt-36 lg:px-36 lg:pt-32 md:px-24 md:pt-24 sm:px-16 sm:pt-52 px-8 pt-32'
+export const sectionPadding = '2xl:px-96 2xl:pt-40 xl:px-60 xl:pt-36 lg:px-48 lg:pt-36 md:px-24 md:pt-24 sm:px-16 sm:pt-52 px-8 pt-32'
 
-export const headerOneFontSize = 'lg:text-9xl md:text-7xl text-5xl'
+export const headerOneFontSize = 'xl:text-9xl lg:text-7xl md:text-6xl text-5xl'
 
 export const headerTwoFontSize = 'lg:text-5xl md:text-4xl text-3xl'
 
